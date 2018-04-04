10f2dbfda0d8009d776a6854b890916fe7530eab
sara <sarahll.pro@gmail.com>
2018-04-04 13:03:07 +0200

    Commit12
    Controller Ok pour l'affichage de ma page Index, avec un lien vers mon formulaire

--- src/main/java/com/formation/dating/controllers/ControllerDating.java
+++ src/main/java/com/formation/dating/controllers/ControllerDating.java
@@ -1,13 +1,43 @@
 package com.formation.dating.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.formation.dating.entities.Utilisateur;
+import com.formation.dating.services.UtilisateurService;
+
 @Controller
 public class ControllerDating {
-	@GetMapping(value = "/") // arrivé à la racine de mon projet
+	
+	private final UtilisateurService us;
+	
+	@Autowired
+	public ControllerDating(UtilisateurService us) {
+		super();
+		this.us = us;
+	}
+	
+	@GetMapping(value="/formulaire") //Juste le Root(URL)
+	public ModelAndView inscription() {
+		ModelAndView mav=new ModelAndView("formulaire.html");//l'html qui sera affichée
+		
+		mav.addObject("utilisateur", new Utilisateur());
+		
+		return mav;
+		
+	}
+	
+	}
+	
+	
+
+
+
+
+	/*@GetMapping(value = "/") // arrivé à la racine de mon projet
 	public ModelAndView index() { // creer une methode qui nous retourne notre index.html
 		return new ModelAndView("index.html");
-	}
-}
+	}*/
+
